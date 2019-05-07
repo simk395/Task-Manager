906287f73a2b54380dc62d483440fa8f55f10674
Simon Kong <simon.kong95@gmail.com>
2019-05-07 12:34:56 -0400

    seperate router files

---
+++ src/routers/task.js
@@ -0,0 +1,66 @@
+const Task = require('../models/task')
+const express = require('express')
+const router = new express.Router()
+
+router.get('/tasks', async (req,res) => {
+    try {
+        const task = await Task.find({})
+        res.send(task)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.get('/tasks/:id', async (req,res) => {
+    const _id = req.params.id
+   try {
+       const task = await Task.findById(_id)
+       res.send(task)
+   } catch (error) {
+       res.status(500).send()
+   }
+})
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const update = Object.keys(req.body)
+    const allowedUpdates = ["description", "completed"]
+    const isValid = update.every(update => allowedUpdates.includes(update))
+
+    if (!isValid) {
+        return res.status(400).send({ error: "Invalid argument!" })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            res.status(404).send()
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            res.status(404).send()
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
